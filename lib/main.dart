62ea84339d3f268ffe83ae6902586e8b0848f071
raufurrahim <ffrifatrahman@gmail.com>
2026-08-03 02:40:06 +0600

    Hide debug bannar

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,7 @@ class RentEase extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'RentEase',
       theme: ThemeData(
         // This is the theme of your application.
