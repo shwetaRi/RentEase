16a501b2d3a1c9c86e1da30180466db7545a4d43
Shweta Biswas <shwetahere04@gmail.com>
2026-08-05 00:01:26 +0600

    initial commit: setup RentEase project

--- lib/main.dart
+++ lib/main.dart
@@ -106,7 +106,7 @@ class _MyHomePageState extends State<MyHomePage> {
           children: [
             const Text('You have pushed the button this many times:'),
             Text(
-              '$_counter',
+              '$counter',
               style: Theme.of(context).textTheme.headlineMedium,
             ),
           ],
