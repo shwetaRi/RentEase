08849cf3d4caa37ed78b6a0816b264e60563229b
raufurrahim <ffrifatrahman@gmail.com>
2026-09-23 22:45:10 +0600

    Set up Firebase integration

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:project_rent_ease/screens/dashboard_page.dart';
 import 'package:project_rent_ease/screens/home_page.dart';
 import 'package:project_rent_ease/screens/property_details_page.dart';
+import 'package:project_rent_ease/screens/signUp.dart';
 import 'screens/login_page.dart';
+import 'firebase_options.dart';
 import 'screens/property_details_page.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const RentEase());
 }
 
